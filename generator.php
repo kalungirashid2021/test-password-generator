dce8df6eb4dfd9a167a01eb940ed44a602e32627
Umar <www.balikowaumar@gmail.com>
2022-09-13 15:18:50 -0700

    initial commit

---
+++ generator.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>password generator</title>
+</head>
+<body>
+    <form action="">
+
+    <label for="email">email</label>
+        <input type="email"><br>
+        <label for="pass">password</label>
+        <input type="password"> <br> <br>
+        <input type="submit" value="generate password">
+        <input type="submit" value="submit password">
+    </form>
+</body>
+</html>
